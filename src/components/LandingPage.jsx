dca1185994d8ff61743545ea1156043fcbbaef30
Limp Reload <emanuelquintanilla85@gmail.comm>
2022-09-19 14:16:03 -0300

    primeraModificacion

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom"
 export default function Landing(){
     return (
         <div>
-            <h1>Bienvenidos!</h1>
+            <h1>Bienvenidos a la tierra C-137!</h1>
             <Link to="/home">
                 <button> Home</button>
             </Link>
